bcf21d75726bb45a784f97982307ae35c7a149e1
screeт <horizonjournal78@gmail.com>
2023-10-09 12:14:31 +0300

    Рефакторинг класса UserRepository.java

--- src/main/java/rborodin/skillgram/userservice/repository/UserRepository.java
+++ src/main/java/rborodin/skillgram/userservice/repository/UserRepository.java
@@ -4,15 +4,9 @@ package rborodin.skillgram.userservice.repository;
 import org.springframework.data.repository.CrudRepository;
 import rborodin.skillgram.userservice.entity.User;
 
-import javax.transaction.Transactional;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User, Long> {
-    
-    Optional<User> findById(UUID uuid);
-    @Transactional
-    void deleteById(UUID uuid);
+public interface UserRepository extends CrudRepository<User, UUID> {
 
-    boolean existsById(UUID id);
+
 }
